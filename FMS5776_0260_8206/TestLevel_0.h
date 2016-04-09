bbd1c983a9c499ff9bb9879586ef7fb6192f8844
desdo09 <desdo09@hotmail.com>
2016-04-10 01:19:06 +0300

    Allocate functio added. deallocate function added. some bugs fixed. algorithms types fixed. recursia in allocate function does not work. some commits added.

---
+++ FMS5776_0260_8206/TestLevel_0.h
@@ -0,0 +1,114 @@
+#pragma once
+#include "Header.h"
+#include "Disk.h"
+
+class TestLevel_0
+{
+	
+public:
+
+	string diskName = "TheDisk";
+	string ownerName = "Da&Jo";
+	Disk d;
+
+	TestLevel_0()
+	{
+
+	}
+
+	TestLevel_0(string name,string owner)
+	{
+		string diskName = name;
+		string ownerName = owner;
+	}
+
+	~TestLevel_0()
+	{
+		cout << "\n\n\n\nDTOR\n\n\n\n\n";
+	}
+
+	void printStructSize()
+	{
+		cout << "start" << endl;
+		cout << "Size Of Disk -->" << sizeof(Disk) << endl;
+		cout << "Size Of Sector -->" << sizeof(Sector) << endl;
+		cout << "Size Of VolumeHeader -->" << sizeof(VolumeHeader) << endl;
+		cout << "Size Of DAT -->" << sizeof(DAT) << endl;
+		cout << "Size Of DirEntry -->" << sizeof(DirEntry) << endl;
+		cout << "Size Of SectorDir -->" << sizeof(SectorDir) << endl;
+		cout << "Size Of FileHeader -->" << sizeof(FileHeader) << endl;
+
+		cout << "Size Of RootDir -->" << sizeof(RootDir) << endl;
+	}
+
+	void printDiskInfo()
+	{
+		VolumeHeader* vh = &d.vhd;
+
+		cout << "	disk name:        " << vh->diskName << endl;
+		cout << "	Owner Name:       " << vh->diskOwner << endl;
+		cout << "	prodDate:         " << vh->prodDate << endl;
+		cout << "	formatDate:       " << vh->formatDate << endl;
+		cout << "	isFormated:       " << vh->isFormated << endl;
+		cout << "	addrDataStart:    " << vh->addrDataStart << endl;
+
+		cout << "	ClusQty:          " << vh->clusQty << endl;
+		cout << "	dataClusQty:      " << vh->dataClusQty << endl;
+
+		cout << "	addrDAT:          " << vh->addrDAT << endl;
+		cout << "	addrRootDir:      " << vh->addrRootDir << endl;
+		cout << "	addrDATcpy:       " << vh->addrDATcpy << endl;
+		cout << "	addrRootDirCpy:   " << vh->addrRootDirCpy << endl << endl;
+
+	}
+
+	void test_create()
+	{
+	
+	//	cout << "\npre createdisk: " << endl;
+		//printDiskInfo();
+	//	cout << "post createdisk: " << endl;
+		d.createdisk(diskName, ownerName);
+	//	printDiskInfo();
+		cout << "Disk created!" << endl;
+	}
+
+	void test_mount()
+	{
+
+		//cout << "\npre mountdisk: " << endl;
+	//	printDiskInfo();
+		this->d.mountdisk(diskName);
+	//	cout << "post mountdisk: " << endl;
+	//	printDiskInfo();
+	//	d.unmountdisk();
+		cout << "mounted!" << endl;
+	}
+
+
+	void test_rwSector()
+	{
+
+		Sector sector;
+		d.mountdisk(diskName);
+
+		cout << "\nread sector: " << endl;
+		d.readSector(8, &sector);
+		strcpy_s(sector.rawData, "this is write temp sector");
+		d.writeSector(8, &sector);
+		d.unmountdisk();
+
+	}
+
+
+	void test_0()
+	{
+
+		
+
+		printStructSize();
+		test_create();
+		test_mount();
+
+	}
+};
